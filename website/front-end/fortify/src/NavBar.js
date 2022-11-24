676c4253bdc3e1762bc060015aa21e4dc0b2d7ae
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2022-11-24 13:42:46 +1100

    complet the navbar

--- website/front-end/fortify/src/NavBar.js
+++ website/front-end/fortify/src/NavBar.js
@@ -7,15 +7,30 @@ const NavBar = () => {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Fortify</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Organizations</Nav.Link>
-              <Nav.Link href="#link">Volunteer</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Fortify
+          </Navbar.Brand>
+
+          <Nav className="justify-content-center ">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Organizations</Nav.Link>
+            <Nav.Link href="#link">Volunteer</Nav.Link>
+            <Nav.Link href="#link">Search</Nav.Link>
+          </Nav>
+          <Nav className="SLNav">
+            <Nav.Link href="#link">Login</Nav.Link>
+
+            <Nav.Link className="NBtn" href="#link">
+              Sign Up
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
     </>
