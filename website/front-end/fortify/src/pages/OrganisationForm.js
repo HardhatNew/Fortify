cbe697e3f1981cd7ecb8bb3192d3d1fb656f8c72
gnawd <zqwa@deakin.edu.au>
2022-12-02 15:59:49 +1100

    Started on OrganisationForm

--- website/front-end/fortify/src/pages/OrganisationForm.js
+++ website/front-end/fortify/src/pages/OrganisationForm.js
@@ -6,9 +6,35 @@ More details goes here....
 what the from about 
 what supposed to do
 */
+import React from 'react'
 import '../component/Organisation/style.css'
 
 const OrganisationForm = () => {
-  return <p className="Organization">Organisation Form</p>
+  return (
+  <>
+  <div className='center'>
+  <div className='Card'>
+  <div className='upperContainer'>
+    <div className='imageContainer'>
+      <img src="https://logos-world.net/wp-content/uploads/2022/04/Coles-Supermarkets-Logo-1991.png" alt='' height="150px" width="300px" />
+    </div>
+  </div>
+  <div className='lowerContainer'>
+    <h3> Coles </h3>
+    <h4> Victoria 3149 </h4>
+  </div>
+  </div>
+  </div>
+
+  <div className='center2'>
+  <div className='Card2'>
+  <div className='upperContainer2'>
+    <h3> Contacts </h3>
+    </div>
+  </div>
+  </div>
+
+  </>
+  )
 }
 export default OrganisationForm
