d6a1af06f9fdc77437a8370815779cce2fa32e48
Beatrice <brerimoi@deakin.edu.au>
2022-12-03 07:45:38 +1100

    Volunteer section

--- website/front-end/fortify/src/component/homepage/Volunteer.js
+++ website/front-end/fortify/src/component/homepage/Volunteer.js
@@ -6,8 +6,41 @@ More details goes here....
 what the from about 
 what supposed to do
 */
+import volunteerImage from "../../images/volunteer-image.png";
 
 const Volunteer = () => {
-  return <div className="volunteerSection"></div>
+  return <div className="volunteerSection">
+    <div style={{
+      textAlign: "center",
+    }}>
+    <h2>Volunteer</h2>
+    </div>
+    
+    <div className="volunteer-grid">
+    <article>
+      <ul>
+        <li className="number">1</li>
+        <li className="list-heading">Benefit 1</li>
+        <li className="list-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, tempora.</li>
+      </ul>
+      <ul>
+        <li className="number">2</li>
+        <li className="list-heading">Benefit 2</li>
+        <li className="list-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, tempora.</li>
+      </ul>
+      <ul>
+        <li className="number">3</li>
+        <li className="list-heading">Benefit 3</li>
+        <li className="list-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, tempora.</li>
+      </ul>
+
+      <button className="btn-green">Join Us</button>
+    </article>
+
+    <article>
+      <img src={volunteerImage} alt="" />
+    </article>
+    </div>
+  </div>
 }
 export default Volunteer
