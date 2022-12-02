041606691ddfb3c19815489fbc2fabcaa8026b9e
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2022-12-02 16:44:58 +1100

    create the sign up form and the function to collect the data after submited

--- website/front-end/fortify/src/component/navbar/SignUp.js
+++ website/front-end/fortify/src/component/navbar/SignUp.js
@@ -1,8 +1,35 @@
+// author Ali Al-kinani
+//2/12/2022
+
+/* 
+More details goes here....
+what the from about 
+what supposed to do
+*/
+
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 import './style.css'
+import { useState } from 'react'
+
 const SignUpPage = (props) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [fullName, setFullName] = useState('')
+  const [comments, setComments] = useState('')
+  const [signAS, setSignAs] = useState('')
+  const submitForm = () => {
+    let setData = {}
+    setData.fullName = fullName
+    setData.password = password
+    setData.comments = comments
+    setData.email = email
+    setData.signAS = signAS
+    console.log('info ', setData)
+    props.onHide()
+  }
+
   return (
     <Modal
       className="modalSignUp"
@@ -18,23 +45,45 @@ const SignUpPage = (props) => {
         <Form>
           <Form.Group className="mb-3" controlId="FullName">
             <Form.Label>Full Name</Form.Label>
-            <Form.Control type="text" placeholder="Full Name" autoFocus />
+            <Form.Control
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              autoFocus
+              onChange={(event) => setFullName(event.target.value)}
+              value={fullName}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              onChange={(event) => setEmail(event.target.value)}
+              value={email}
+            />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formPlaintextPassword">
+          <Form.Group className="mb-3" controlId="password">
             <Form.Label>Password</Form.Label>
 
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={(event) => setPassword(event.target.value)}
+              value={password}
+            />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formPlaintextPassword">
+          <Form.Group className="mb-3" controlId="confirmPassword">
             <Form.Label>Confirm Password</Form.Label>
 
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Confirm Password" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formPlaintextPassword">
+          <Form.Group
+            className="mb-3"
+            controlId="signAS"
+            onChange={(event) => setSignAs(event.target.value)}
+            value={signAS}
+          >
             <Form.Label>Sign As</Form.Label>
 
             <Form.Check
@@ -42,22 +91,29 @@ const SignUpPage = (props) => {
               label="Company"
               name="signAs"
               type="radio"
+              value="company"
             />
             <Form.Check
               id="volunteer"
               label="Volunteer"
               name="signAs"
               type="radio"
+              value="volunteer"
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Comments</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control
+              as="textarea"
+              rows={3}
+              onChange={(event) => setComments(event.target.value)}
+              value={comments}
+            />
           </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="success" onClick={props.onHide}>
+        <Button variant="success" onClick={submitForm}>
           SignUp
         </Button>
         <Button variant="danger" onClick={props.onHide}>
