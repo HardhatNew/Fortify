46b24cd16f3c176805bbee5017d9fd5e0652b4a3
gnawd <zqwa@deakin.edu.au>
2022-12-02 23:52:22 +1100

    Did the About Us column by trying to format the same way as the template.

--- website/front-end/fortify/src/pages/OrganisationForm.js
+++ website/front-end/fortify/src/pages/OrganisationForm.js
@@ -11,25 +11,32 @@ import '../component/Organisation/style.css'
 import Envelope from '../images/envelope-fill.svg'
 import Phone from '../images/phone-fill.svg'
 import Telephone from '../images/telephone.png'
+import SmileyFace from '../images/emoji-smile.png'
 
 const OrganisationForm = () => {
   return (
   <>
-  <div className='center'>
-  <div className='Card'>
-  <div className='upperContainer'>
-    <div className='imageContainer'>
-      <img src="https://logos-world.net/wp-content/uploads/2022/04/Coles-Supermarkets-Logo-1991.png" alt='' height="150px" width="300px" />
-    </div>
-  </div>
-  <div className='lowerContainer'>
-    <h3> Coles </h3>
-    <h4> Victoria 3149 </h4>
-  </div>
-  </div>
-  </div>
+  <div className='row'>
+    <div className='column'>
+  <table>
+    <tr>
+      <td>
+        <div className='Card'>
+        <div className='upperContainer'>
+          <div className='imageContainer'>
+            <img src="https://logos-world.net/wp-content/uploads/2022/04/Coles-Supermarkets-Logo-1991.png" alt='' height="150px" width="300px" />
+          </div>
+        </div>
+        <div className='lowerContainer'>
+          <h3> Coles </h3>
+          <h4> Victoria 3149 </h4>
+        </div>
+        </div>
+      </td>
+      </tr>
 
-  <div className='center2'>
+<tr>
+  <td>
   <div className='Card2'>
   <div className='upperContainer2 '>
   <img src={Telephone} alt="Telephone" />
@@ -45,8 +52,35 @@ const OrganisationForm = () => {
     </div>
   </div>
   </div>
-  </div>
+  </td>
+  </tr>
+  </table>
+</div>
 
+
+<div className='column'>
+<table>
+  <tr>
+    <td>
+  <div className='center3'>
+  <div className='Card3'>
+  <div className='upperContainer2 '>
+  <img src={SmileyFace} alt="SmileyFace" />
+  <h3 className='inlineposition'> About Us</h3>
+    <div className='lowerContainer'>
+    <br />
+    <h3 className='inlineposition smalltext'>Coles is a leading Australian retailer, with over 2,500 retail outlets nationally. Coles makes life easier for Australians by delivering quality, value and service. We process more than 20 million customer transactions each week, providing our customers with products from thousands of farmers and suppliers. </h3>
+    <br />
+    <br />
+    </div>
+  </div>
+  </div>
+  </div>
+  </td>
+  </tr>
+  </table>
+  </div>
+  </div>
   </>
   )
 }
