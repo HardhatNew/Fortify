1f28875d06cae467650d6b3ba97b57359220d7e4
Harry\har <singhhard@deakin.edu.au>
2022-12-05 19:48:01 +1100

    Create the contact us section by Harry

--- website/front-end/fortify/src/component/homepage/ContactUs.js
+++ website/front-end/fortify/src/component/homepage/ContactUs.js
@@ -8,6 +8,51 @@ what supposed to do
 */
 
 const ContactUs = () => {
-  return <div className="contactUs"></div>
+  return (
+   <>
+     <div className="contactUs">
+      <h1 className="contactB">Contact</h1>
+      <p className="sty">Let's stay connected</p>
+      <p className="styT">Its never been eaiser to get in touch with Fortify. Call us, use our live chat widget or email <br /> and we'll get back to you as soon as possible! </p>
+      <h3 className="c">
+        <p id="email">
+          <p className="emailIcon"><i class="fa-sharp fa-solid fa-envelope-open"></i></p>
+          Email
+          <p className="EmailID">contact@fortify.com</p>
+        </p>
+        <p id="phone">
+          <p className="phoneIcon"> <i className="fa-solid fa-phone"></i></p>
+          Phone
+          <p className="phoneNumber">+7-843-672-431</p>
+        </p>
+        <p id="social">
+          <p className="socialIcon"><i className="fa-solid fa-group-arrows-rotate"></i></p>
+          Socials
+          <p className="socialIcons">
+             <i className="fa-brands fa-facebook-f first"></i>
+
+             <i className="fa-brands fa-twitter second"></i>
+
+             <i className="fa-brands fa-instagram third"></i>
+              
+             <i className="fa-brands fa-linkedin fourth"></i>
+          </p>
+        </p>
+      </h3>
+    </div>
+    <div className="form">
+        <p className="contactFormName">Email</p>
+        <input type="text" className="emailInput" placeholder="example@post.com"/>
+        <br/><br/>
+
+        <p className="contactFormName">Message</p>
+        <textarea type="text" className="message" placeholder="Your message here"></textarea>
+        <input type="button" className="submitBUtton" value="Send"/>
+        
+    </div>
+    {/* <div className="footer"></div> */}
+   
+   </>
+  )
 }
 export default ContactUs
